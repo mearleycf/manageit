3f417ffcca31bc4650586ecbb03e28ce98195a78
Mike Earley <mike@5sn1.com>
2024-06-23 10:46:03 -0400

    Integrated prettier, including installing packages and creating rc and ignore files. Updated .gitignore. Installed sitemap, tailwind, vercel, fonts, icons, node globals for eslint. Updated tailwind.config.mjs to include fonts. Updated tsconfig.json to have correct compiler options, paths, include and exclude paths. Created global.css and added tailwind stuff and inter font stuff.

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    singleAttributePerLine: true,
+    arrowParens: 'avoid',
+    plugins: [
+      'prettier-plugin-astro',
+      'prettier-plugin-packagejson',
+      'prettier-plugin-organize-attributes',
+      'prettier-plugin-sort-imports',
+      'prettier-plugin-astro-organize-imports',
+      'prettier-plugin-tailwindcss',
+    ],
+    attributeGroups: ['$CODE_GUIDE'],
+  }
+  
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel()
+});
